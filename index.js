849f9ea2f310146aa52a5ac67746a1d89dd32152
Pt <dracarys1312@gmail.com>
2016-08-21 22:21:13 +0700

    :bug: add header cross origin

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ var logger = require('./helpers/logger');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
+
 // add-on swagger-editor
 app.use('/swagger', express.static('./node_modules/swagger-editor'));
 app.use('/', express.static('./docs'));
